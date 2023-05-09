95a7786f197cabc20866d883562c24fe71f660d5
daniel <danielrubensdaniel@gmail.com>
2023-05-09 17:28:04 -0300

    feat: maximize window

--- bot/steps.py
+++ bot/steps.py
@@ -7,3 +7,6 @@ class Bot:
 
     def land_page(self):
         self.navigator.open_available_browser(self.url)
+
+    def maximize_window(self):
+        self.navigator.maximize_browser_window()
--- run.py
+++ run.py
@@ -2,9 +2,8 @@ from bot.steps import Bot
 
 def main():
     bot = Bot('https://www.nytimes.com/')
-    test = bot.land_page()
-    print(test)
-
+    bot.land_page()
+    bot.maximize_window()
 
 if __name__ == '__main__':
     main()

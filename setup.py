98872ae02ec251da4fdee8d641d2743ff95038c3
daniel <danielrubensdaniel@gmail.com>
2023-05-10 20:52:14 -0300

    setup: venv and gitignore

--- setup.py
+++ setup.py
@@ -1,13 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="rpa_robocorp",
     description="RPA Robocorp",
     install_requires=[
-        "parsel==1.7.0",
-        "requests==2.24.0",
+        "requests==2.3.0",
         "python-decouple==3.3",
     ],
-    setup_requires=["pytest-runner"],
-    tests_require=["pytest"],
+    packages=find_packages(exclude=['tests']),
 )

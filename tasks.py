056339714e6bf48274ee15ddd0c6f84d3e856f43
daniel <danielrubensdaniel@gmail.com>
2023-05-26 16:47:56 -0300

    refactor: call work items on robot yaml and tasks

--- tasks.py
+++ tasks.py
@@ -2,7 +2,8 @@ import time
 from bot.steps import Bot
 from bot.writer import Writer
 from functools import wraps
-from initial_values import Constants
+# from initial_values import Constants
+from RPA.Robocorp.WorkItems import WorkItems
 
 def handle_exceptions_and_delay(func):
     @wraps(func)
@@ -17,17 +18,25 @@ def handle_exceptions_and_delay(func):
 
 @handle_exceptions_and_delay
 def main():
-    bot = Bot(Constants.URL)
+    wi = WorkItems()
+    wi.get_input_work_item()
+
+    URL = wi.get_work_item_variable("URL")
+    SEARCH = wi.get_work_item_variable("SEARCH")
+    SECTION = [wi.get_work_item_variable("SECTION")]
+    TYPE = [wi.get_work_item_variable("TYPE")]
+
+    bot = Bot(URL)
     bot.land_page()
     bot.maximize_window()
     bot.click_cookies()
     bot.click_search()
     time.sleep(2)
-    bot.search_field(Constants.SEARCH)
+    bot.search_field(SEARCH)
     time.sleep(2)
-    bot.click_filter_section(Constants.SECTION)
+    bot.click_filter_section(SECTION)
     time.sleep(2)
-    bot.click_filter_type(Constants.TYPE)
+    bot.click_filter_type(TYPE)
     bot.select_date(2)
     news = bot.get_results()
     writer = Writer(news)
